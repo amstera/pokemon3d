fc8d2bc04dad3c12c244d0278ecc9765d700127b
tylergreen1993 <tyler-green@live.com>
2021-04-24 13:43:25 -0500

    Added walking and NPCs

---
+++ Assets/Scripts/CameraTracker.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraTracker : MonoBehaviour
+{
+    public GameObject Target;
+    private Vector3 _offset;
+
+    void Start()
+    {
+        _offset = transform.position - Target.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Target.transform.position + _offset;
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed = 3.5f;
+    private Direction _direction;
+    public Animator Animator;
+
+    void Update()
+    {
+        Direction newDirection = Direction.None;
+        bool isWalking = false;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newDirection = Direction.East;
+            transform.position += Vector3.forward * Speed * Time.deltaTime;
+            isWalking = true;
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newDirection = Direction.West;
+            transform.position += Vector3.back * Speed * Time.deltaTime;
+            isWalking = true;
+        }
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newDirection = Direction.North;
+            transform.position += Vector3.left * Speed * Time.deltaTime;
+            isWalking = true;
+        }
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newDirection = Direction.South;
+            transform.position += Vector3.right * Speed * Time.deltaTime;
+            isWalking = true;
+        }
+
+        if (Animator.GetBool("IsWalking") != isWalking)
+        {
+            Animator.SetBool("IsWalking", isWalking);
+        }
+
+        if (_direction != newDirection && newDirection != Direction.None)
+        {
+            int angleChange = ((int)newDirection - (int)_direction) * 90;
+            transform.Rotate(0, 0, angleChange, Space.Self);
+            _direction = newDirection;
+        }
+    }
+
+    private enum Direction
+    {
+        None = -1,
+        North = 0,
+        West = 1,
+        South = 2,
+        East = 3
+    }
+}
